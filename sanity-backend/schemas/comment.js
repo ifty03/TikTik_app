4fb2bcad6e18ad0f49a72f2693bff0acd5987afa
ifty03 <ashikulislamb84@gmail.com>
2022-08-01 23:13:39 +0600

    first commit

---
+++ sanity-backend/schemas/comment.js
@@ -0,0 +1,17 @@
+export default {
+  name: "comment",
+  title: "Comment",
+  type: "document",
+  fields: [
+    {
+      name: "postedBy",
+      title: "posted by",
+      type: "postedBy",
+    },
+    {
+      name: "comment",
+      type: "Comment",
+      type: "string",
+    },
+  ],
+};
---
+++ sanity-backend/schemas/postedBy.js
@@ -0,0 +1,6 @@
+export default {
+  name: "postedBy",
+  title: "posted by",
+  type: "reference",
+  to: [{ type: "user" }],
+};
